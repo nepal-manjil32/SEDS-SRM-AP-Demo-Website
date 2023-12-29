70a4007d629f052710abea05b35791739b6cd26e
nepal-manjil32 <110888950+nepal-manjil32@users.noreply.github.com>
2023-12-29 13:21:50 +0545

    Add files via upload

---
+++ codeforce.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int t, a, b, c;
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b >> c;
+        if(a != b && a != c) cout << a << endl;
+        else if(b != a && b != c) cout << b << endl;
+        else cout << c << endl;
+    }
+    return 0;
+}
